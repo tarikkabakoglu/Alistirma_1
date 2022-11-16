df3638a9ed5f3362cf99a27df9bca3fbb399961c
Tarık Kabakoğlu <114907478+tarikkabakoglu@users.noreply.github.com>
2022-11-16 20:11:33 +0300

    Update soru_2.py

--- soru_2.py
+++ soru_2.py
@@ -1,14 +1,10 @@
-from math import sqrt
+import math
 
-i=0
-X=[]
-while True:
-    i=i+1
-    pikare=1/(i**2)
-    pi=sqrt(pikare/6)
-    X.append(pi)
-
-    if sum(X)>=3.149:
-        break
-
-print(sum(X))
+toplam=0
+for i in range(1,10000000):
+    toplam+=(1/(i*i))
+pi=(toplam*6)**(0.5)
+pi2=math.sqrt(toplam*6)
+print(pi)
+print(pi2)
+print(math.pi)
